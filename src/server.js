47ed3337e41c412696d25ad72ee679a354e4a6da
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2023-10-24 12:49:24 -0300

    final configuration adjusments

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: [process.env.PORT, 'http://127.0.0.1:5173'],
   credentials: true
 }));
 
